75a3e36d3bf81c3cf93a809a9c06192670503f22
Kristin Zenger <kristin.zenger@t-online.de>
2020-11-17 14:20:50 +0100

    refactoring styling

--- resources/views/posts/header.blade.php
+++ resources/views/posts/header.blade.php
@@ -2,28 +2,26 @@
 <html>
     <head>
         <title>Daily Thoughts</title>
-        <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet"> -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
+
         <!-- Styles -->
         <style>
             #header {
                 background-color: #eeeeee; 
-            }       
+            }      
         </style>
     </head>
     <body>
         <div class="container">
-            <div class = "row mb-3 py-2"  id="header">
-                <div class="col">
+            <div class = "row mb-4 py-2 pl-4" id="header">
                 @if (Request::url() === "http://laravelprojectsm.local/posts" || Request::url() === "http://laravelprojectsm.local/index") 
-                    <h3>Daily Thoughts</h3>
+                    <h3><strong>Daily Thoughts</strong></h3>
                 @else
                     <a class="btn btn-outline-secondary" href="{{ route('posts.index') }}">Zu den Posts</a>
                 @endif                    
-                </div>
-            </div>         
-            @yield('content')
+            </div>   
+            @yield('content')    
         </div>
     </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,32 +1,19 @@
 @extends("posts.header")
 
 @section('content')
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Styles -->
+    <style>
+        body {
+            font-family: 'Raleway';
+        }
+        h1 {
+            color: #708FB3;
+        }
+    </style>
 
-        <title>Daily Thoughts</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            body {
-                font-family: 'Raleway';
-            }
-            h1 {
-                text-align: center;
-                color: #708FB3;
-            }
-        </style>
-    </head>
-
-    <body class="antialiased">
-        <div class="container">
+        <div class="row justify-content-center">
             <h1>Willkommen</h1>
+        </div>
 
             @if ($message = Session::get('success'))
 
@@ -36,6 +23,7 @@
 
             @endif
 
+            <!-- Icon from https://icons.getbootstrap.com/ -->
             <div class=row>
                 <div class="col offset-10">
                     <a class="btn btn-info rounded-circle py-2 mb-4" href="{{ route('posts.create') }}"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -71,7 +59,4 @@
             @endforeach
 
             {{ $posts->links() }}
-        </div>
-    </body>
-</html>
 @endsection
