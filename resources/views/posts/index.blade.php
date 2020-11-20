b7a4c6b9a3800727d11d9ac582ad7dfd5c18f9d2
Kristin Zenger <kristin.zenger@t-online.de>
2020-11-20 15:10:35 +0100

    adjusting layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,30 +12,33 @@
     </style>
 
         <!-- Personalisierte Begrüßung -->
+        @if (null !== ( Auth::user() ))
         <div class="row justify-content-center">
-            @if (null !== ( Auth::user() ))
             <h1>Willkommen  {{ Auth::user()->name }}!</h1>
-            @else
+        </div>
+        <!-- Icon from https://icons.getbootstrap.com/ -->
+        <div class=row>
+            <div class="col offset-10">
+                <a class="btn btn-info rounded-circle py-2 mb-4" href="{{ route('posts.create') }}"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                </svg></a>
+            </div>
+        </div>
+        @else
+        <div class="row justify-content-center">
             <h1>Willkommen</h1>
-            @endif
+        </div>
+        @endif
+
+        @if ($message = Session::get('success'))
+
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
         </div>
 
-            @if ($message = Session::get('success'))
+        @endif
 
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-
-            @endif
-
-            <!-- Icon from https://icons.getbootstrap.com/ -->
-            <div class=row>
-                <div class="col offset-10">
-                    <a class="btn btn-info rounded-circle py-2 mb-4" href="{{ route('posts.create') }}"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                    </svg></a>
-                </div>
-            </div>
+            
 
             @foreach ($posts as $post)
 
